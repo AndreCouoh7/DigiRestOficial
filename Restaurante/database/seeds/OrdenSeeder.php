194df90557f1b36bb995400a06879f4b6c6c9e97
AndreCouoh7 <andrecouohdzul@gmail.com>
2019-11-28 20:06:47 -0600

    eliminando

--- Restaurante/database/seeds/OrdenSeeder.php
+++ Restaurante/database/seeds/OrdenSeeder.php
@@ -11,6 +11,7 @@ class OrdenSeeder extends Seeder
      */
     public function run()
     {
+       
         DB::table('usuarios')->insert([
             'nombre'=>'Berlin',
             'apellido' =>'Programing',
@@ -37,6 +38,14 @@ class OrdenSeeder extends Seeder
             'fechaNacimiento' => 02/02/1989,
              'correo' => 'cielo@gmail.com', 
              'tipo'=> 'Cliente'
+        ]);
+        DB::table('usuarios')->insert([
+            'nombre' => 'Joel',
+            'apellido'=> 'Pech',
+             'direccion'=>'C-17',
+              'telefono' => '9971386229',
+              'fechaNacimiento' => '1998-11-09',
+              'correo' => 'joel@gmail.com'
         ]);
         DB::table('categorias')->insert([
             'nombre'=> 'Bebidas'
@@ -85,13 +94,32 @@ class OrdenSeeder extends Seeder
             'precio' => '15'
         ]);
 
-        DB::table('usuarios')->insert([
-            'nombre' => 'Joel',
-            'apellido'=> 'Pech',
-             'direccion'=>'C-17',
-              'telefono' => '9971386229',
-              'fechaNacimiento' => '1998-11-09',
-              'correo' => 'joel@gmail.com'
-        ]);
+       
+        DB::table('meseros')->insert([
+            'idUsuario' => 1
+        ]) ;
+        DB::table('asignaciones')->insert([
+            'fechaAsignacion'=>'12/02/2019',
+             'idMesa' => 1,
+              'idMesero'=>1
+        ]) ;
+        DB::table('tickets')->insert([
+            'iva'=>'5%',
+            'total'=>'90',
+            'idOrden'=>1
+        ]) ;
+        DB::table('clientes')->insert([
+            'idUsuario'=> 2
+        ]) ;
+        DB::table('cajeros')->insert([
+            'idUsuario' => 3
+        ]) ;
+        DB::table('pagos')->insert([
+            'cantidadPago'=> 89,
+             'fechaPago'=>'15/09/2019',
+              'idCajero'=> 1,
+               'idCliente'=>1
+        ]) ;
+        
     }
 }
