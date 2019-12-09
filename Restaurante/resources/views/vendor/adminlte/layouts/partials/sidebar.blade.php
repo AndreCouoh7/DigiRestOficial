e556c73089fb0b9a26bfcd367c28149f08e6b776
AndreCouoh7 <andrecouohdzul@gmail.com>
2019-12-09 11:18:06 -0600

    Editando Menu

--- Restaurante/resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ Restaurante/resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -35,14 +35,71 @@
             <!-- Optionally, you can add icons to the links -->
             <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
             <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
+                    {{-- Meseros --}}
             <li class="treeview">
-                <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <a href="#"><i class='fa fa-link'></i> <span>Meseros</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
+                    <li><a href="{{url('/meseros/create')}}">Crear Mesero</a></li>
+                    <li><a href="{{ url('/meseros')}}">Mostrar Meseros</a></li>
+                    <li><a href="/"></a></li>
+                    
                 </ul>
             </li>
+        
+            <li class="treeview">
+                <a href="#"><i class='fa fa-link'></i> <span>Cajeros</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="{{url('/cajeros/create')}}">Agregar Cajeros</a></li>
+                    <li><a href="{{ url('/cajeros')}}">Mostrar Cajeros</a></li>
+                    
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#"><i class='fa fa-link'></i> <span>Asignaciones</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="{{url('/asignaciones/create')}}">Crear Asignacion</a></li>
+                    <li><a href="{{ url('/asignaciones')}}">Mostrar Asignaciones</a></li>
+                    
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#"><i class='fa fa-link'></i> <span>Clientes</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="{{url('/clientes/create')}}">Agregar Cliente</a></li>
+                    <li><a href="{{ url('/clientes')}}">Mostrar Clientes</a></li>
+                    
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#"><i class='fa fa-link'></i> <span>Usuarios</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="{{url('/usuarios/create')}}">Crear Usuario</a></li>
+                    <li><a href="{{ url('/usuarios')}}">Mostrar Usuarios</a></li>
+                    
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#"><i class='fa fa-link'></i> <span>Productos</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="{{url('/productos/create')}}">Agregar Productos</a></li>
+                    <li><a href="{{ url('/productos')}}">Mostrar Productos</a></li>
+                    
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#"><i class='fa fa-link'></i> <span>Ordenes</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="{{url('/ordenes/create')}}">Crear Orden</a></li>
+                    <li><a href="{{ url('/ordenes')}}">Mostrar Ordenes</a></li>
+                    
+                </ul>
+            </li>
+        
+        
+        
+
         </ul><!-- /.sidebar-menu -->
+       
     </section>
     <!-- /.sidebar -->
 </aside>
