934ccff0b16ff283a715fe339031722e6ae90568
AndreCouoh7 <andrecouohdzul@gmail.com>
2019-12-12 08:35:10 -0600

    Corrigiendo errores de testregistrarTest

--- Restaurante/tests/Unit/RegistrarOrdenTest.php
+++ Restaurante/tests/Unit/RegistrarOrdenTest.php
@@ -2,34 +2,35 @@
 namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-class ExampleTest extends TestCase
+class RegistrarOrdenTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testregistrarTest()
     {
         $orden =  array(
-            'mesaid' => 1,
-            'detalle'=>array([
+            'idMesa' => 1,
+            'detalleordenes'=>array('a'=>[
                 'idProducto' =>1,
                 'cantidad' => 2,
                 'precio'    => 12.50,
             ],
-            [
+            'b'=>[
                 'idProducto' =>2,
                 'cantidad' => 1,
                 'precio'    => 9.50,
             ])
         
         );
-        $response=$this->json('POST',
-		'/V1/IRegistrarOrden/orden',
-		$orden
-		);
         
+        $response = $this->json('POST','api/V1/IRegistrarOrden/orden',$orden );
+        //Esperando Respuesta del servidor
+        $response->assertStatus(200);
+        //Validando que exista en ka base de datos
+
 $this->assertDatabaseHas('ordenes', [
     'idMesa' => '1',
 ]);
